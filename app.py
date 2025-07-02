3cd55a6e7215a34c780b02fde20eec1866c8bfce
unknown <151481930+Sebas-lop@users.noreply.github.com>
2025-07-01 23:17:34 -0500

    update proyect

--- app.py
+++ app.py
@@ -46,6 +46,85 @@ def registrar():
     return render_template("registro.html")
 
 
+
+
+@app.route('/nosotros')
+def nosotros():
+    return render_template('nosotros.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/login_paciente', methods=["GET", "POST"])
+def login_paciente():
+    if request.method == "POST":
+        dni = request.form["dni"]
+        contrasena = request.form["contrasena"]
+
+        usuario = consultas.obtener_usuario_paciente_por_dni(dni)
+
+        if usuario and usuario["contrasena_user"] == contrasena:
+            session["usuario_id"] = usuario["paciente_id"]
+            session["nombre"] = usuario["nombre"]
+            session["rol"] = "paciente"
+            flash("Inicio de sesión exitoso", "success")
+            return redirect(url_for('pagina_paciente'))
+        else:
+            flash("DNI o contraseña incorrectos", "danger")
+
+    return render_template("login_paciente.html")
+
+
+
+@app.route('/login_medico', methods=["GET", "POST"])
+def login_medico():
+    if request.method == "POST":
+        email = request.form["email"]
+        contrasena = request.form["contrasena"]
+
+        usuario = consultas.obtener_usuario_medico_por_email(email)
+
+        if usuario and usuario["contrasena_user"] == contrasena:  # O usa check_password_hash si está encriptada
+            session["usuario_id"] = usuario["medico_id"]
+            session["nombre"] = usuario["nombre"]
+            session["rol"] = "medico"
+            flash("Inicio de sesión exitoso " , "success")
+            return redirect(url_for('pagina_medico'))
+        else:
+            flash("Email o contraseña incorrectos", "danger")
+
+    return render_template("login_medico.html")
+
+
+@app.route('/pagina_paciente')
+def pagina_paciente():
+    if 'usuario_id' in session and session.get('rol') == 'paciente':
+        return render_template('pagina_paciente.html', nombre=session['nombre'])
+    else:
+        flash("Acceso denegado. Solo para pacientes.", "danger")
+        return redirect(url_for('login_paciente'))
+
+@app.route('/pagina_medico')
+def pagina_medico():
+    if 'usuario_id' in session and session.get('rol') == 'medico':
+        return render_template('pagina_medico.html', nombre=session['nombre'])
+    else:
+        flash("Acceso denegado. Solo para médicos.", "danger")
+        return redirect(url_for('login_medico'))
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    flash("Has cerrado sesión correctamente", "info")
+    return redirect(url_for('login'))
+
+
+
+if __name__ == '__main__':
+    print("Iniciando Flask en http://localhost:5000")
+    app.run(debug=True)
+
 '''@app.route('/registro', methods=["GET", "POST"])
 def registrar():
     if request.method == "POST":
@@ -64,30 +143,6 @@ def registrar():
         return redirect("/bienvenida")
     
     return render_template("registro.html")'''
-
-
-@app.route('/nosotros')
-def nosotros():
-    return render_template('nosotros.html')
-
-@app.route('/login')
-def login():
-    return render_template('login.html')
-
-@app.route('/login_paciente')
-def login_paciente():
-    return render_template('login_paciente.html')
-
-@app.route('/login_medico')
-def login_medico():
-    return render_template('login_medico.html')
-
-
-
-if __name__ == '__main__':
-    print("Iniciando Flask en http://localhost:5000")
-    app.run(debug=True)
-
 '''@app.route('/guardar_paciente', methods=['POST'])
 def guardar_paciente():
     nombre = request.form['nombre']
--- consultas.py
+++ consultas.py
@@ -5,10 +5,16 @@ from bd import obtener_conexion
 
  
 def insertar_usuario_y_paciente(nombre, apellido, dni, fecha_nacimiento, sexo, telefono, direccion, email, contrasena, id_rol):
+    # Formatear nombre y apellido en InitCap
+    nombre = nombre.strip().capitalize()
+    apellido = apellido.strip().capitalize()
+
+    # También puedes limpiar otros campos si quieres:
+    email = email.strip().lower()
+
     conexion = obtener_conexion()
     try:
         with conexion.cursor() as cursor:
-            # Si es PACIENTE (id_rol == 1), insertar en tabla Paciente
             id_paciente = None
             id_medico = None
 
@@ -28,7 +34,6 @@ def insertar_usuario_y_paciente(nombre, apellido, dni, fecha_nacimiento, sexo, t
                 cursor.execute(sql_medico, (nombre, apellido, email, 'activo'))
                 id_medico = cursor.lastrowid
 
-            # Insertar en Usuario_Sistema
             sql_usuario = """
                 INSERT INTO Usuario_Sistema (nombre_user, contrasena_user, email_user, id_rol, id_paciente, id_medico)
                 VALUES (%s, %s, %s, %s, %s, %s)
@@ -38,6 +43,42 @@ def insertar_usuario_y_paciente(nombre, apellido, dni, fecha_nacimiento, sexo, t
         conexion.commit()
     finally:
         conexion.close()
+
+        
+        
+def obtener_usuario_paciente_por_dni(dni):
+    conexion = obtener_conexion()
+    try:
+        with conexion.cursor(pymysql.cursors.DictCursor) as cursor:
+            sql = """ SELECT us.*, p.nombre, p.id_paciente AS paciente_id
+                FROM Usuario_Sistema us
+                JOIN Paciente p ON us.id_paciente = p.id_paciente
+                WHERE p.dni = %s AND us.id_rol = 1 """
+
+            cursor.execute(sql, (dni,))
+            return cursor.fetchone()
+    finally:
+        conexion.close()
+        
+def obtener_usuario_medico_por_email(email):
+    conexion = obtener_conexion()
+    try:
+        with conexion.cursor(pymysql.cursors.DictCursor) as cursor:
+            sql = """
+                SELECT us.*, m.nombre, m.id_medico AS medico_id
+                FROM Usuario_Sistema us
+                JOIN Medico m ON us.id_medico = m.id_medico
+                WHERE us.email_user = %s AND us.id_rol = 2
+            """
+            cursor.execute(sql, (email,))
+            return cursor.fetchone()
+    finally:
+        conexion.close()
+
+      
+
+        
+            
    
 '''def insertar_paciente(nombre, apellido, dni, fecha_nacimiento, sexo, telefono, direccion, email):
     conexion = obtener_conexion()
